a4dadbf616a1444766134611578067794de25e7b
pavol.szetei <pavol.szetei@thermofisher.com>
2021-09-09 10:44:11 +0200

    rename test class

--- IntToRomanTests/IntToRomanConverterTest.cs
+++ IntToRomanTests/IntToRomanConverterTest.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace IntToRomanTests
 {
-    public class UnitTest1
+    public class IntToRomanConverterTest
     {
         [Fact]
         public void ToRoman_Zero_Return_EmptyString()
